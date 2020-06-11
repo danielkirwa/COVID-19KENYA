9089451d279ce940d7e33788a45751fbfe72cbcb
danielkirwa <kirwadan03@gmail.com>
2020-06-11 18:19:39 +0300

    modified file

--- Graphs.js
+++ Graphs.js
@@ -1,5 +1,13 @@
 function bargraph() {
 	// body...
-	var n = 4565;
+	 n = document.getElementById('newcases').text;
 	console.log(n);
+}
+function loaddate(argument) {
+	// body...
+	var today = new Date();
+
+	var date = (today.getMonth()+1)+'-'+today.getDate()+'-'+today.getFullYear();
+	document.getElementById('currentdate').innerHTML = date;
+	console.log(date);
 }
